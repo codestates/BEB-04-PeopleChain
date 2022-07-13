f4fafb18dea34312862475f397b1ccba8fe6d7b4
eunminCho <silvermn99@gmail.com>
2022-07-14 02:44:17 +0900

    feat: add tags in MeetingCreate

--- memint/src/pages/MeetingPage/MeetingCreate.js
+++ memint/src/pages/MeetingPage/MeetingCreate.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   View,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 import {TextInput} from 'react-native-paper';
 import BackButton from '../../components/BackButton';
@@ -14,6 +15,7 @@ import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useToast} from '../../utils/hooks/useToast';
 import SingleModal from '../../components/SingleModal';
+import TagElement from './TagElement';
 
 function MeetingCreate() {
   const [submittable, setSubmittable] = useState(false);
@@ -25,10 +27,10 @@ function MeetingCreate() {
     peopleNum: undefined,
     tags: [],
   });
+  console.log(meetingInfo)
   const [confirmModalVisible, setConfirmModalVisible] = useState(false);
   const navigation = useNavigation();
   const {showToast} = useToast();
-
   const RegionDropDownData = [
     {label: '서울 전체', value: 1},
     {label: '강남구', value: 2},
@@ -53,6 +55,16 @@ function MeetingCreate() {
     {label: '3:3', value: 3},
     {label: '4:4', value: 4},
   ];
+  const tagData = {
+    mood: [
+      '부어라 마셔라',
+      '부어라 마셔라1',
+      '부어라 마셔라2',
+      '부어라 마셔라3',
+    ],
+    topic: ['연애', '연애1', '연애2', '연애2'],
+    alcoholType: ['소주', '소주1', '소주2', '소주3'],
+  };
 
   useEffect(() => {
     const {title, description, region, peopleNum} = meetingInfo;
@@ -117,6 +129,7 @@ function MeetingCreate() {
           <RNDateTimePicker
             value={meetingInfo.date}
             mode="datetime"
+            locale="ko"
             style={styles.datepicker}
             onChange={(event, date) =>
               setMeetingInfo({...meetingInfo, date: date})
@@ -133,6 +146,10 @@ function MeetingCreate() {
               items={RegionDropDownData}
               value={meetingInfo.region}
               style={{
+                inputIOS: {
+                  fontSize: 16,
+                  color: 'black',
+                },
                 placeholder: {
                   fontSize: 16,
                   color: 'gray',
@@ -152,6 +169,10 @@ function MeetingCreate() {
               items={PeopleDropDownData}
               value={meetingInfo.peopleNum}
               style={{
+                inputIOS: {
+                  fontSize: 16,
+                  color: 'black',
+                },
                 placeholder: {
                   fontSize: 16,
                   color: 'gray',
@@ -160,7 +181,6 @@ function MeetingCreate() {
             />
             <Icon name="arrow-drop-down" size={19} color={'gray'} />
           </TouchableOpacity>
-
           <TouchableOpacity
             onPress={() => {
               navigation.navigate('InviteFriend');
@@ -168,8 +188,49 @@ function MeetingCreate() {
             <Text style={styles.text}>친구 초대하기</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.createElement}>
-          <Text style={styles.text}>태그</Text>
+        <View style={styles.tagElement}>
+          <Text style={[styles.text, styles.tagTitle]}>태그</Text>
+          <View style={styles.tagsContainer}>
+            <View style={styles.tagCategory}>
+              <Text style={styles.tagCategoryTitle}>분위기</Text>
+              <ScrollView style={styles.tags} horizontal={true}>
+                {tagData.mood.map((tag, idx) => (
+                  <TagElement
+                    key={idx}
+                    tag={tag}
+                    meetingInfo={meetingInfo}
+                    setMeetingInfo={setMeetingInfo}
+                  />
+                ))}
+              </ScrollView>
+            </View>
+            <View style={styles.tagCategory}>
+              <Text style={styles.tagCategoryTitle}>주제</Text>
+              <ScrollView style={styles.tags} horizontal={true}>
+                {tagData.topic.map((tag, idx) => (
+                  <TagElement
+                    key={idx}
+                    tag={tag}
+                    meetingInfo={meetingInfo}
+                    setMeetingInfo={setMeetingInfo}
+                  />
+                ))}
+              </ScrollView>
+            </View>
+            <View style={styles.tagCategory}>
+              <Text style={styles.tagCategoryTitle}>술</Text>
+              <ScrollView style={styles.tags} horizontal={true}>
+                {tagData.alcoholType.map((tag, idx) => (
+                  <TagElement
+                    key={idx}
+                    tag={tag}
+                    meetingInfo={meetingInfo}
+                    setMeetingInfo={setMeetingInfo}
+                  />
+                ))}
+              </ScrollView>
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
@@ -214,6 +275,10 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     height: 60,
   },
+  tagElement: {
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+  },
   text: {
     fontSize: 16,
     color: 'gray',
@@ -227,6 +292,23 @@ const styles = StyleSheet.create({
   selectButton: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  tagTitle: {
+    marginTop: 10,
+  },
+  tagsContainer: {
+    marginBottom: 10,
+  },
+  tags: {
+    flexDirection: 'row',
+  },
+  tagCategoryTitle: {
+    marginTop: 5,
+    marginBottom: 10,
+    color: 'gray',
+  },
+  tagCategory: {
+    marginVertical: 5,
   },
 });
 
