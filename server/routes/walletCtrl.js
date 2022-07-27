5af9b51b4ee1737b16a3aa91cb80c816ca8ecef8
96sunghyun <visioner2168@gmail.com>
2022-07-27 16:40:00 +0900

    feat: success make LCNToETH function

--- server/routes/walletCtrl.js
+++ server/routes/walletCtrl.js
@@ -6,7 +6,6 @@ const contractAddress = require("../contracts/ERC20/ERC20address");
 const { SERVER_PRIVATEKEY, SERVER_ADDRESS, ETH_NETWORK } = process.env;
 const provider = new HDWalletProvider(SERVER_PRIVATEKEY, ETH_NETWORK);
 const web3 = new Web3(provider);
-const ERC20Contract = new web3.eth.Contract(abi, contractAddress);
 const { toWei, fromWei } = web3.utils;
 const myContract = new web3.eth.Contract(abi, contractAddress);
 
@@ -14,6 +13,7 @@ const myContract = new web3.eth.Contract(abi, contractAddress);
 // 토큰 가격은 0.001eth === 1LCN으로 고정한 상태로 진행한다.
 const ETHToLCN = async (req, res) => {
   const { address, privateKey, ETHAmount } = req.body;
+
   try {
     // 먼저 거래 전 사용자의 eth 잔액을 확인한다.
     const beforeBalance = await web3.eth.getBalance(address);
@@ -70,7 +70,73 @@ const ETHToLCN = async (req, res) => {
   }
 };
 
-const LCNToETH = async (req, res) => {};
+const LCNToETH = async (req, res) => {
+  const { address, privateKey, tokenAmount } = req.body;
+
+  try {
+    // address에 tokenAmount만큼의 LCN이 있는지 확인한다.
+    // 정확히 비교하려면 아래 beforeBalance에 toWei를 덮어준 후 비교해야 한다. 그렇지만 지금은 일단 생략.
+    const beforeBalance = await myContract.methods.balanceOf(address).call();
+
+    // user의 지갑에서 server지갑으로 tokenAmount만큼 LCN을 transfer하는 함수
+    const getLCNFromUser = async () => {
+      const gasPrice = await web3.eth.getGasPrice();
+      const data = myContract.methods
+        .transfer(SERVER_ADDRESS, toWei(tokenAmount.toString()))
+        .encodeABI();
+
+      const rawTx = {
+        to: contractAddress,
+        gas: 2000000,
+        gasPrice,
+        data,
+      };
+
+      const signedTx = await web3.eth.accounts.signTransaction(
+        rawTx,
+        privateKey
+      );
+
+      return await web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        async (err, result) => {
+          if (err) return res.status(400).send({ error: err });
+          return result;
+        }
+      );
+    };
+
+    // server의 account에서 user의 account로, 알맞은 양의 ETH를 지급하는 함수
+    const sendETHToUser = async () => {
+      const gasPrice = await web3.eth.getGasPrice();
+      return await web3.eth.sendTransaction({
+        from: SERVER_ADDRESS,
+        to: address,
+        gas: 2000000,
+        gasPrice,
+        value: toWei((tokenAmount / 1000).toString()),
+      });
+    };
+
+    // LCN 받는 함수 실행 후, 이어서 ETH를 보내는 함수 실행
+    getLCNFromUser()
+      .then(() => {
+        return sendETHToUser();
+      })
+      .then(async () => {
+        const ETHBalance = await web3.eth.getBalance(address);
+        const LCNBalance = await myContract.methods.balanceOf(address).call();
+        res.send({
+          // 아래 나와있는 balance들은 최종 업데이트 된 결과값이다.
+          // 아래 값을 firestore에 저장하고, front에 success 라는 res를 보내면 front에서 정보를 업데이트하여 렌더링하면 될 것 같다.
+          ETHBalance: fromWei(ETHBalance.toString()),
+          LCNBalance: fromWei(LCNBalance.toString()),
+        });
+      });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const toOffChain = async (req, res) => {};
 
