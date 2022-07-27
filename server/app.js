511ec7b8d1b02dc5641165304decce3c72b9ffc9
js_lee <jongsuklee45@gmail.com>
2022-07-27 15:55:42 +0900

    feat: firebase init

--- server/app.js
+++ server/app.js
@@ -8,12 +8,33 @@ const app = express();
 const fs = require("fs");
 const port = 5000;
 
+//Firebase setting
+const admin = require("firebase-admin");
+const firestore = require("firebase-admin/firestore");
+const serviceAccount = require("./key/memint-c2130-firebase-adminsdk-p6jhw-39f8a3b04b.json");
+
+const firebaseApp = admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
+
+const db = firestore.getFirestore(firebaseApp);
+//Firebase setting
+
+// test();
+// async function test() {
+// 	db.collection("cities").doc("LA2").set({
+// 		name: "Los Angeles 2",
+// 		state: "CA 2",
+// 		country: "USA 2",
+// 	});
+// }
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello Web3");
+	res.send("hello Web3");
 });
 
 app.use("/auth", auth);
@@ -21,23 +42,23 @@ app.use("/wallet", wallet);
 app.use("/transaction", transactionRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err["status"] = 404;
-  next(err);
+	const err = new Error("Not Found");
+	err["status"] = 404;
+	next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: {
-      message: err.message,
-    },
-  });
+	res.status(err.status || 500);
+	res.json({
+		errors: {
+			message: err.message,
+		},
+	});
 });
 
 app.listen(port, () => {
-  console.log(`Memint Server listening at http://localhost:${port}`);
+	console.log(`Memint Server listening at http://localhost:${port}`);
 });
 
 module.exports = app;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,23 +3,37 @@ const auth = express.Router();
 const Web3 = require("web3");
 require("dotenv").config();
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_NETWORK));
-// const { getFirestore } = require('firebase-admin/firestore');
-// const db = getFirestore()
-
+const { getFirestore } = require("firebase-admin/firestore");
+// const db = getFirestore();
+const app = require("../app");
 // 회원가입 시 user의 db에 address, privateKey를 추가해주는 함수
-auth.route("/register").get(async (req, res) => {
-  try {
-    // const { id } = req.body.id;
-    const account = await web3.eth.accounts.create();
-
-    console.log(account);
-    // await db.collection('User').doc(id).set({
-    // address : account.address, privateKey : account.privateKey
-    // })
-    res.status(200).send(account);
-  } catch (error) {
-    res.status(404).send(error);
-  }
+auth.route("/register").post(async (req, res) => {
+	try {
+		// const { id } = req.body.id;
+		console.log("db is");
+		console.log(app);
+		console.log(app.db);
+		const account = await web3.eth.accounts.create();
+		console.log(account);
+		console.log(req.body.id);
+		await db.collection("User").doc(req.body.id).set({
+			// await db.collection("User").doc(id).set({
+			address: account.address,
+			privateKey: account.privateKey,
+		});
+		console.log("debug");
+		// test();
+		// async function test() {
+		// 	db.collection("cities").doc("LA2").set({
+		// 		name: "Los Angeles 2",
+		// 		state: "CA 2",
+		// 		country: "USA 2",
+		// 	});
+		// }
+		res.status(200).send(account);
+	} catch (error) {
+		res.status(404).send(error);
+	}
 });
 
 module.exports = auth;
